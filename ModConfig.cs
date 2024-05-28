a8b8d1a33317758b4f1da1c6445b63d04c81046e
HeyImAmethyst <HeyImAmethyst44@gmail.com>
2024-05-27 20:40:33 -0400

    Added comments to modConfig

--- ModConfig.cs
+++ ModConfig.cs
@@ -10,17 +10,22 @@ namespace ArsVenefici
 {
     public class ModConfig
     {
-        /// <summary>The button to open the spell book menu.</summary>
-        public SButton OpenSpellBookButton { get; set; }
-
-        public SButton MoveCurrentSpellLable { get; set; } = SButton.OemTilde;
-
         /// <summary>The pixel position at which to draw the spell lable, relative to the top-left corner of the screen.</summary>
         public Point Position { get; set; } = new(10, 10);
 
+        /// <summary>Press Shift and this button to open the spell book.</summary>
+        public SButton OpenSpellBookButton { get; set; }
+
+        /// <summary>Press Shift and this button to move the spell lable.</summary>
+        public SButton MoveCurrentSpellLable { get; set; } = SButton.OemTilde;
+
+        /// <summary>The button to move to the next spell. Press Shift and this button to move to the next shape group on the selected spell.</summary>
         public SButton NextSpellButton { get; set; } = SButton.X;
+
+        /// <summary>The button to move to the previous spell. Press Shift and this button to move to the previous shape group on the selected spell.</summary>
         public SButton PreviousSpellButton { get; set; } = SButton.Z;
 
+        /// <summary>The button to cast the current spell.</summary>
         public SButton CastSpellButton { get; set; } = SButton.Q;
 
         public ModConfig()
